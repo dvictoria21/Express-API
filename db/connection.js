f593cfbff49baad3818619c88734ff9f18dc57f4
dvictoria21 <watt.daija@yahoo.com>
2023-07-25 12:07:29 -0400

    Updating for Deployment

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const databaseUrl = "mongodb://127.0.0.1:27017/brooklynNets";
+
+const databaseUrl = process.env.DB_URL ||
+"mongodb://127.0.0.1:27017/brooklynNets";
 
 mongoose.set("returnOriginal", false);
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import db from "./db/connection.js";
 import routes from "./routes/index.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.json());
