df08203a626080a4a47ef68c740fd108f30de9dd
dvictoria21 <watt.daija@yahoo.com>
2023-08-04 10:04:04 -0400

    added cors...i think

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import db from "./db/connection.js";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.json());
 app.use("/", routes);
+app.use(
+  cors({
+    origin: "*",
+  })
+)
 
 async function startServer() {
   try {
