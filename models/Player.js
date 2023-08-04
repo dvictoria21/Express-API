c309e0955db49c4510e6318b3afc8981be39d289
dvictoria21 <watt.daija@yahoo.com>
2023-08-04 09:39:45 -0400

    added images

--- models/Player.js
+++ models/Player.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 
 const playerSchema = new Schema(
   {
+    img: { type: String },
     name: { type: String },
     team: { type: String },
     draftClass: { type: String },
     position: { type: String },
+    "height(in)": {type: Number},
     gp: { type: Number },
     min: { type: Number },
     fgp: { type: Number },
